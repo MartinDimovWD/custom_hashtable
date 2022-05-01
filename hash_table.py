4742489f1878d9dd98c016ce66746df839ab9107
MartinDimovWD <75526884+MartinDimovWD@users.noreply.github.com>
2022-05-01 19:33:20 +0300

    File Initialization

---
+++ hash_table.py
@@ -0,0 +1,18 @@
+
+class HashTable:
+    def __init__(self):
+        self.array = [None]*4
+
+    def hash(self, key):
+        pass
+
+    def add(self, key, value):
+        pass
+
+    def get(self,key):
+        pass
+
+
+table = HashTable()
+print(table.array)
+table['name'] = 'Peter'
